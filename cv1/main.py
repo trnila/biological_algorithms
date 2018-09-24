8a8bc188cfeb1a6ea66320d00983be0abb773613
Daniel Trnka <daniel.trnka@gmail.com>
2018-09-24 22:28:42 +0200

    cv1 heap algo

--- cv1/main.py
+++ cv1/main.py
@@ -8,18 +8,31 @@ import sys
 # vypsat cas do ktereho to spocita
 
 
-def trajectories(cities):
+def impl_reclist(cities):
     if len(cities) < 1:
         yield []
     else:
-        for perm in trajectories(cities[1:]):
+        for perm in impl_reclist(cities[1:]):
             for i in range(len(cities)):
                 yield list(perm[:i]) + [cities[0]] + list(perm[i:])
+
+def impl_heap(cities):
+    def fn(cities, n):
+        if n == 1:
+            yield cities
+        else:
+            for i in range(len(cities)):
+                yield from fn(cities, n - 1)
+                if n & 1 == 0:
+                    cities[i], cities[n - 1] = cities[n - 1], cities[i]
+                else:
+                    cities[0], cities[n - 1] = cities[n - 1], cities[0]
+    yield from fn(cities, len(cities))
 
 
 def print_all(N, permutate_fn):
     start = time.time()
-    cities = range(N)
+    cities = list(range(N))
 
     print(f"Available trajectories for {N} cities")
     for trajectory in permutate_fn(cities):
@@ -30,12 +43,13 @@ def print_all(N, permutate_fn):
 
 implementations = {
     'itertools': itertools.permutations,
-    'recursive': trajectories
+    'reclist': impl_reclist,
+    'heap': impl_heap,
 }
 
 parser = argparse.ArgumentParser()
 parser.add_argument("N", help="maximal number of cities", default=4, type=int, nargs='?')
-parser.add_argument("--perm-impl", default='recursive', choices=implementations.keys())
+parser.add_argument("--perm-impl", default='reclist', choices=implementations.keys())
 
 args = parser.parse_args()
 
